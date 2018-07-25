d0602c2e140d96a7585c2234caaba3e23a2e75a4
Amenda Hing <amendahing@gmail.com>
2018-07-25 15:25:59 -0700

    added collapsible info section for adoption component

--- src/app/adoption/adoption.component.ts
+++ src/app/adoption/adoption.component.ts
@@ -8,11 +8,37 @@ import { AdoptionService } from "../services/adoption.service";
   styleUrls: ["./adoption.component.scss"]
 })
 export class AdoptionComponent implements OnInit {
+  children: any;
+  expanded = [false, false, false];
+
   constructor(private adoptionService: AdoptionService) {}
 
   ngOnInit() {
-    this.adoptionService.getPosts().subscribe(posts => {
-      console.log(posts);
-    });
+    // this.adoptionService.getPosts().subscribe(posts => {
+    //   console.log(posts);
+    // });
+  }
+
+  expandInfo(index) {
+    if (this.expanded[index] === true) {
+      var icon = document.getElementsByClassName("collapse")[index];
+      var dropBox = document.getElementsByClassName("adoption-drop-down")[
+        index
+      ];
+
+      dropBox.style.display = "none";
+      icon.setAttribute("class", "expand");
+
+      this.expanded[index] = false;
+    } else if (this.expanded[index] === false) {
+      var icon = document.getElementsByClassName("expand")[index];
+      var dropBox = document.getElementsByClassName("adoption-drop-down")[
+        index
+      ];
+
+      dropBox.style.display = "block";
+      icon.setAttribute("class", "collapse");
+      this.expanded[index] = true;
+    }
   }
 }
